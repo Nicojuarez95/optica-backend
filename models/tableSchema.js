d8dd85728d3fc9b23d7757b09ef4cf175aaa6fb9
Nico <juareznico61@gmail.com>
2024-10-01 12:55:48 -0300

    back pedidosbar

--- models/tableSchema.js
+++ models/tableSchema.js
@@ -1,6 +1,5 @@
 import mongoose from 'mongoose';
 
-// Definir el esquema de usuario
 const tableSchema = new mongoose.Schema({
     number: { type: Number, required: true, unique: true },
     status: { type: String, enum: ['available', 'occupied'], default: 'available' }
